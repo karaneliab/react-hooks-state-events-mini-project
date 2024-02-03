ee8f43f4708c90b151b4a1a8c29c81cf20c457da
karaneliab <eliabkaran7884@gmail.com>
2024-02-03 15:16:35 +0300

    left pending

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,18 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory,setSelectedCategory] = useState("All")
+  
+function handleCategoryChange(category){
+  setSelectedCategory(category)
+}
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter  categories={CATEGORIES} selectedCategory={selectedCategory} onChangeSelectedCategory ={handleCategoryChange}/>
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS} selectedCategory={selectedCategory}/>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
-
-function TaskList() {
+import Task from "./Task"
+function TaskList({tasks,selectedCategory}) {
+  const filteredTasks = selectedCategory === "All" ? tasks : tasks.filter((task) => 
+  task.category === selectedCategory
+  )
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+  {filteredTasks.map ((task)=>(
+     <Task key = {task.text} text = {task.text} category = {task.category}/>
+  ))}
+   
     </div>
   );
 }
